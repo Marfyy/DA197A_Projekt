73c15c6c7d5846058991ebcdbba2fef9fa1925e6
snowdaowg <rickardogb@gmail.com>
2014-03-25 16:14:20 +0100

    12 Signed-off-by: snowdaowg <rickardogb@gmail.com>

---
+++ Filmarkiv/Filmarkiv/Home.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="Filmarkiv.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="height: 462px">
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+    
+        
+        <center><asp:TextBox ID="txt_search" runat="server" Width="833px"></asp:TextBox><br />
+        <asp:Button ID="btn_search" OnClick="btn_search_Click" runat="server" Text="Search" Width="396px"/><br />
+        </center><br /><br /><br /><br />
+        <asp:Label ID="lbl_title" runat="server" Text=""></asp:Label><br />
+        <asp:Label ID="lbl_year" runat="server" Text=""></asp:Label><br />
+        <asp:Label ID="lbl_rated" runat="server" Text=""></asp:Label><br />
+        <asp:Label ID="lbl_released" runat="server" Text=""></asp:Label><br />
+        <asp:Label ID="lbl_runtime" runat="server" Text=""></asp:Label><br />
+        <asp:Label ID="lbl_genre" runat="server" Text=""></asp:Label><br />
+        <asp:Label ID="lbl_director" runat="server" Text=""></asp:Label><br />
+        <asp:Label ID="lbl_writer" runat="server" Text=""></asp:Label><br />
+        <asp:Label ID="lbl_actors" runat="server" Text=""></asp:Label><br />
+        <asp:Label ID="lbl_plot" runat="server" Text=""></asp:Label><br />
+        <asp:Label ID="lbl_language" runat="server" Text=""></asp:Label><br />
+        <asp:Label ID="lbl_country" runat="server" Text=""></asp:Label>
+        <asp:Label ID="lbl_awards" runat="server" Text=""></asp:Label><br />
+        <asp:Label ID="lbl_metascore" runat="server" Text=""></asp:Label><br />
+        <asp:Label ID="lbl_rating" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Filmarkiv/Filmarkiv/Home.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace Filmarkiv
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_search_Click(object sender, EventArgs e)
+        {
+            string src = txt_search.Text;
+            string apirequest = "http://omdbapi.com/?r=XML&t=" + src;
+            WebClient client = new WebClient();
+            string xmldoc = client.DownloadString(apirequest);
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(xmldoc);
+
+
+            XmlElement root = xml.DocumentElement;
+            XmlNodeList subroot = root.SelectNodes("movie");
+
+            for (int i = 0; i < subroot.Count; i++)
+            {
+
+                XmlNode elem = subroot.Item(i);
+                lbl_title.Text = "Title: " + elem.Attributes["title"].Value;
+                lbl_year.Text = "Year: " + elem.Attributes["year"].Value;
+                lbl_rated.Text = "Rated: " + elem.Attributes["rated"].Value;
+                lbl_released.Text = "Released: " + elem.Attributes["released"].Value;
+                lbl_runtime.Text = "Runtime: " + elem.Attributes["runtime"].Value;
+                lbl_genre.Text = "Genre: " + elem.Attributes["genre"].Value;
+                lbl_director.Text = "Director: " + elem.Attributes["director"].Value;
+                lbl_writer.Text = "Writer: " + elem.Attributes["writer"].Value;
+                lbl_actors.Text = "Actors: " + elem.Attributes["actors"].Value;
+                lbl_plot.Text = "Plot: " + elem.Attributes["plot"].Value;
+                lbl_language.Text = "Language: " + elem.Attributes["language"].Value;
+                lbl_country.Text = "Country: " + elem.Attributes["country"].Value;
+                lbl_awards.Text = "Awards: " + elem.Attributes["awards"].Value;
+                lbl_metascore.Text = "Metascore: " + elem.Attributes["metascore"].Value;
+                lbl_rating.Text = "IMDB Rating: " + elem.Attributes["imdbRating"].Value;
+                
+            }
+            
+        }
+
+        
+        
+    }
+}
